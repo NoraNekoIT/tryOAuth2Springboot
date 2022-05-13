8cede5fe93bed9b5556498c7dcfbd33fb7168298
NoraNekoIT <bunsmart18@gmail.com>
2022-05-13 17:18:20 +0700

    push

---
+++ src/main/java/com/example/cobaoauth2springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.cobaoauth2springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CobaOAuth2SpringbootApplication.class);
+    }
+
+}
---
+++ src/test/java/com/example/cobaoauth2springboot/CobaOAuth2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cobaoauth2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaOAuth2SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
